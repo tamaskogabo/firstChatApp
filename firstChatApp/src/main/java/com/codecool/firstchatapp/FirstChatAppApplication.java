8407bd561a7dc336fb2e7d04f7e29090ac232e8c
Gabor Tamasko <117508570+tamaskogabo@users.noreply.github.com>
2023-07-05 14:43:42 +0200

    create starter project

---
+++ firstChatApp/src/main/java/com/codecool/firstchatapp/FirstChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.firstchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstChatAppApplication.class, args);
+    }
+
+}
---
+++ firstChatApp/src/test/java/com/codecool/firstchatapp/FirstChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.firstchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
