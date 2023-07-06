38ace53f85440645bbb1e56ec3c52dea33f4d0ab
Gabor Tamasko <117508570+tamaskogabo@users.noreply.github.com>
2023-07-06 11:02:10 +0200

    start implementing ChatRoom component

--- firstchatappfrontend/src/components/ChatRoom.jsx
+++ firstchatappfrontend/src/components/ChatRoom.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
+import { over } from 'stompjs';
+import SockJS from 'sockjs-client';
 
 export default function ChatRoom() {
-  return <div>ChatRoom</div>;
+    const [userData, setUserData] = useState({
+        username: '',
+        receiverName: '',
+        connected: false,
+        message: '',
+    });
+
+    function handleUserName(e) {
+        setUserData({ ...userData, username: e.target.value });
+    }
+
+    function registerUser() {
+
+    }
+
+    return (
+        <div className='container'>
+            {userData.connected ? (
+                <div></div>
+            ) : (
+                <div className='register'>
+                    <input
+                        id='user-name'
+                        placeholder='Enter a user name'
+                        value={userData.username}
+                        onChange={handleUserName}
+                    />
+                    <button type='button' onClick={registerUser}>
+                        Register
+                    </button>
+                </div>
+            )}
+        </div>
+    );
 }
